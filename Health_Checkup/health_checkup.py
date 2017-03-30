732b5e9805d00392629bf6404002521d42d976d7
jackson <pankajackson@live.co.uk>
2017-03-30 17:13:42 +0530

    Updated: Health Checkup Script, Added: CPU/Memory Alert

--- Health_Checkup/health_checkup.py
+++ Health_Checkup/health_checkup.py
@@ -7,6 +7,8 @@ from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 import socket
 import subprocess
+import time
+import operator
 
 
 mail_from = 'techsupport@thesynapses.com'
@@ -84,12 +86,44 @@ def check_services():
     return service_alert
 
 
+def check_memory():
+    info_dict = {}
+    highusage = 0
+    for x in range(10):
+        mem = str(subprocess.check_output("""free -m | grep Mem""", shell=True))
+        mem_info = mem.split(' ', -1)
+        mem_info = filter(None, mem_info)
+        total = int(mem_info[1].lstrip().rstrip())
+        free = int(mem_info[3].lstrip().rstrip()) + int(mem_info[5].lstrip().rstrip()) + int(mem_info[6].lstrip().rstrip())
+        used = total - free
+        used_per = (float(used)/float(total))*100
+        info = [total, used, free, used_per]
+        info_dict[x] = info
+        time.sleep(1)
+
+    for i in info_dict:
+        print i, info_dict[i]
+        if info_dict[i][3] > 75.0:
+            highusage += 1
+    if highusage > 5:
+        return "RAM USAGE: %s, \nRAM Usage threshold Limit!\n" % max(info_dict.iteritems(), key=operator.itemgetter(1))[1][3]
+
+
+def check_cpu():
+    cpu_usage = psutil.cpu_percent(1, False)
+    if cpu_usage > 75.0:
+        return "CPU USAGE: %s, \nRAM Usage threshold Limit!\n" % cpu_usage
+
+
 def health_check():
     msg = ''
     msg += check_disk()
     msg += check_services()
-
+    msg += check_memory()
+    msg += check_cpu()
     if msg:
-        print msg
+       print msg
     else:
-        print 'All is well!'
+       print 'All is well!'
+
+check_cpu()
